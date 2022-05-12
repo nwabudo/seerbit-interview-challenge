f12ed5db5e964d585a384ce81819d5a4f2de29a4
neoOkpara <emmox55@gmail.com>
2022-05-12 14:49:42 +0100

    Project setup

---
+++ src/main/java/com/seerbit/challenge/SeerbitChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.seerbit.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeerbitChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seerbit/challenge/SeerbitChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seerbit.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

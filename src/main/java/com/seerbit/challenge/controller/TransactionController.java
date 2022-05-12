0c406958b434bac11746d774281e501fc0cde6ad
neoOkpara <emmox55@gmail.com>
2022-05-12 23:07:47 +0100

    Setup completed

---
+++ src/main/java/com/seerbit/challenge/controller/TransactionController.java
@@ -0,0 +1,23 @@
+package com.seerbit.challenge.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @PostMapping()
+    public void postTransaction(){
+
+    }
+
+    @GetMapping()
+    public void getTransaction(){
+
+    }
+
+    @DeleteMapping
+    public void deleteTransaction(){
+
+    }
+}
